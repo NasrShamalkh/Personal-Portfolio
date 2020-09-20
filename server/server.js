a809e365822490ac82ce8ff2382c7e2c2277d3d7
NasrShamalkh <naaaa9sr-@hotmail.com>
2020-09-20 04:33:15 +0300

    fix01

--- server/server.js
+++ server/server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5600;
-app.get('/', (req, res) => {
-  res.send('App only works on production environment ! (try npm run client)');
-});
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
